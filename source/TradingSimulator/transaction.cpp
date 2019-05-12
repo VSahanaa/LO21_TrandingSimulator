b85aee09cedb38172ab5e23a6e565ecac468e205
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2019-05-12 23:12:26 +0200

    create class TransactionManager

--- source/TradingSimulator/transaction.cpp
+++ source/TradingSimulator/transaction.cpp
@@ -1,2 +1,43 @@
 #include "transaction.h"
 
+void TransactionManager::addTransaction(PaireDevises *paire, CoursOHLCV *cours, bool achat, double montant) {
+    double currentMontantBase, currentMontantContrepartie;
+    double montantBase, montantContrepartie;
+
+    if (listeTransaction == nullptr) {
+        currentMontantBase = montantBaseInitial;
+        currentMontantContrepartie = montantContrepartieInitial;
+    }
+    else {
+        currentMontantBase = listeTransaction->getMontantBase();
+        currentMontantContrepartie = listeTransaction->getMontantContrepartie();
+    }
+
+    if (achat) {
+        montantBase = currentMontantBase + montant*cours->getOpen()*(1-pourcentage);
+        montantContrepartie = currentMontantContrepartie - montant;
+        if (montantContrepartie < 0) throw TradingException("montant de devise contre partie n'est pas assez");
+    }
+    else {
+        montantBase = currentMontantBase - montant;
+        montantContrepartie = currentMontantContrepartie + montant/cours->getClose()*(1-pourcentage);
+        if (montantBase<0) throw TradingException("montant de devise de base n'est pas assez");
+    }
+    listeTransaction = new Transaction(listeTransaction, paire, cours, achat, montantBase, montantContrepartie);
+}
+
+void TransactionManager::deleteTransaction() {
+    if (listeTransaction == nullptr) throw TradingException("Il n'y a pas transaction à annuler");
+    Transaction* transactionCurrent = listeTransaction;
+    listeTransaction = transactionCurrent->getTransactionDernier();
+    delete transactionCurrent;
+}
+
+double TransactionManager::solde() const {
+    if (listeTransaction == nullptr) {
+        return montantTotalInitial;
+    }
+    else {
+        return listeTransaction->montantTotal();
+    }
+}
--- source/TradingSimulator/transaction.h
+++ source/TradingSimulator/transaction.h
@@ -5,24 +5,35 @@
 
 class Transaction {
     Transaction* transactionDernier = nullptr;      //organiser comme une liste chainée
-    QDate date;
     PaireDevises* paire;
     CoursOHLCV* cours;
     bool achat;
     double montantBase; //montant actuel de devise de base que l'utilisateur possède
     double montantContrepartie; //montant actuel de devise de contre partie que l'utilisateur possède
 public:
-    Transaction(Transaction* transactionDernier, QDate date, PaireDevises* paire, CoursOHLCV* cours, bool achat, double montantBase, double montantContrepartie)
-        :transactionDernier(transactionDernier), date(date), paire(paire), cours(cours), achat(achat), montantBase(montantBase), montantContrepartie(montantContrepartie) {}
+    Transaction(Transaction* transactionDernier, PaireDevises* paire, CoursOHLCV* cours, bool achat, double montantBase, double montantContrepartie)
+        :transactionDernier(transactionDernier), paire(paire), cours(cours), achat(achat), montantBase(montantBase), montantContrepartie(montantContrepartie) {}
     double differenceBase() const {return this->montantBase - transactionDernier->montantBase;}
     double differenceContrepartie() const {return this->montantContrepartie - transactionDernier->montantContrepartie;}
-    double montantTotal() const {return montantBase*cours->getClose() + montantContrepartie;}
-    double roi(double montantInitial) const {return montantTotal()/montantInitial;}
-    QDate getDate() const {return date;}
+    double montantTotal() const {return montantBase/cours->getClose() + montantContrepartie;}
+    double roi(double montantTotalInitial) const {return montantTotal() / montantTotalInitial;}
+    double getMontantBase() const {return montantBase;}
+    double getMontantContrepartie() const {return montantContrepartie;}
+    Transaction* getTransactionDernier() const {return transactionDernier;}
 };
 
 class TransactionManager {
-
+    Transaction* listeTransaction = nullptr;    //pointeur vers la transaction le plus current, les transactions se organisent comme une liste chainée
+    float pourcentage;
+    double montantBaseInitial;
+    double montantContrepartieInitial;
+    double montantTotalInitial;
+public:
+    //TransactionManager();
+    //~TransactionManager();
+    void addTransaction(PaireDevises* paire, CoursOHLCV* cours, bool achat, double montant);
+    void deleteTransaction();   //supprimer transaction derniere
+    double solde() const;
 };
 
 #endif // TRANSACTION_H
