bfcf5d788edc16977b6fa2e33f62bbb710b1cc09
VSahanaa <50210354+VSahanaa@users.noreply.github.com>
2019-06-10 16:04:10 +0200

    Add files via upload

--- source/TradingSimulator/modeautowidget.h
+++ source/TradingSimulator/modeautowidget.h
@@ -1,27 +1,67 @@
 #ifndef MODEAUTOWIDGET_H
 #define MODEAUTOWIDGET_H
+
+/**
+ * \file 	modeautowidget.h
+ * \brief 	classe ModeAutowidget dans le namespace Ui
+ * 	
+ * 	Contient le QWidget du mode automatique
+ * 			
+ */
+
 #include "Controller/simulation.h"
 #include <QWidget>
 
+//! Namespace Ui
+/**
+* classe : ModeAutowidget
+*/
 namespace Ui {
     class ModeAutowidget;
 }
 
+/**
+* \class ModeAutowidget
+* \brief hérite de QWidget
+* attributs : ui, modeAutomatique, transactionManager
+* méthode publique : ModeAutomatique, ~ModeAutomatique
+* signal public : transactionChanged
+* slots privés : updateData, on_comboBox_timer_currentIndexChanged
+*/
 class ModeAutowidget : public QWidget {
     Q_OBJECT
 public:
+	//! Constructeur explicite
+	/**
+	* \param modeAutomatique :ModeAutomatique*
+	* \param parent :QWidget*
+	*/
     explicit ModeAutowidget(ModeAutomatique* modeAutomatique, QWidget *parent = nullptr);
-    ~ModeAutowidget();
+    //! Destructeur
+	~ModeAutowidget();
 signals:
+	//! signal transactionChanged
+	/**
+	* \return void : signale le changement de transaction (précédente à la suivante)
+	*/
     void transactionChanged();
 private slots:
+	//! slot updateData
+	/**
+	* \return void : pour mettre à jour les données 
+	*/
     void updateData();
+	//! slot on_comboBox_timer_currentIndexChanged
+	/**
+	* \param index :int
+	* \return void : pour le changement au niveau du timer (avancer, reculer...)
+	*/
     void on_comboBox_timer_currentIndexChanged(int index);
 
 private:
-    Ui::ModeAutowidget *ui;
-    ModeAutomatique* modeAutomatique;
-    TransactionManager* transactionManager;
+    Ui::ModeAutowidget *ui; /**< ui :ModeAutowidget* instance de ModeAutowidget*/
+    ModeAutomatique* modeAutomatique; /**< modeAutomatique :ModeAutomatique* pointe sur la classe gérant le mode automatique */
+    TransactionManager* transactionManager; /**< transactionManager :TransactionManager* pointe sur le manager des transactions */
 };
 
 #endif // MODEAUTOWIDGET_H
--- source/TradingSimulator/modepaspaswidget.h
+++ source/TradingSimulator/modepaspaswidget.h
@@ -1,37 +1,93 @@
 #ifndef MODEPASPASWIDGET_H
 #define MODEPASPASWIDGET_H
+
+/**
+ * \file 	modepaspaswidget.h
+ * \brief 	classe modeManuelWidget dans le namespace Ui
+ * 	
+ * 	Contient le QWidget du mode Manuel
+ * 			
+ */
+
 #include "Controller/simulation.h"
 #include <QWidget>
 #include <QMessageBox>
 
+//! Namespace Ui
+/**
+* classe : ModePasPaswidget
+*/
 namespace Ui {
     class ModePasPaswidget;
 }
 
+/**
+* \class ModePasPaswidget
+* \brief hérite de QWidget
+* attributs : ui, modeManuel, transactionManager
+* méthode publique : modePas_Pas, ~ModePas_Pas, updateData
+* signal public : transactionChanged
+* slots privés : on_comboBox_timer_currentIndexChanged, on_goBack_clicked, updateCurrentCours, on_pushButton_achat_clicked, on_pushButton_annulerTransaction_clicked, on_pushButton_vente_clicked
+*/
 class ModePasPaswidget : public QWidget {
     Q_OBJECT
 public:
+	//! Constructeur explicite
+	/**
+	* \param modePas_Pas :ModePas_Pas**
+	* \param parent :QWidget*
+	*/
     explicit ModePasPaswidget(ModePas_Pas* modePas_Pas, QWidget *parent = nullptr);
-    ~ModePasPaswidget();
+    //! Destructeur
+	~ModePasPaswidget();
+	//! slot updateData
+	/**
+	* \return void : pour mettre à jour les données 
+	*/
     void updateData();
 signals:
+	//! signal transactionChanged
+	/**
+	* \return void : signale le changement de transaction (précédente à la suivante)
+	*/
     void transactionChanged();
 private slots:
-
+	//! slot updateCurrentCours
+	/**
+	* \return void : pour la mise à jour après le choix du cours OHLCV
+	*/
     void updateCurrentCours();
+	//! on_goBack_clicked
+	/**
+	* \return void : pour revenir en arrière
+	*/
     void on_goBack_clicked();
+	//! slot on_comboBox_timer_currentIndexChanged
+	/**
+	* \param index :int
+	* \return void : pour le changement au niveau du timer (avancer, reculer...)
+	*/
     void on_comboBox_timer_currentIndexChanged(const QString &arg1);
-
+	//! slot on_pushButton_achat_clicked
+	/**
+	* \return void : pour la demande d'achat
+	*/
     void on_pushButton_achat_clicked();
-
+	//! slot on_pushButton_vente_clicked
+	/**
+	* \return void : pour la demande de vente
+	*/
     void on_pushButton_vente_clicked();
-
+	//! slot on_pushButton_annulerTransaction_clicked
+	/**
+	* \return void : pour la demande d'annuler la transaction
+	*/
     void on_pushButton_annulerTransaction_clicked();
 
 private:
-    Ui::ModePasPaswidget *ui;
-    ModePas_Pas* modePas_Pas;
-    TransactionManager* transactionManager;
+    Ui::ModePasPaswidget *ui; /**< ui :ModePasPaswidget* instance de ModePasPaswidget*/
+    ModePas_Pas* modePas_Pas; /**< modePas_Pas :ModePas_Pas instance de la classe gérant le mode pas à pas*/
+    TransactionManager* transactionManager; /**< transactionManager :TransactionManager* pour gérer les transactions */
 };
 
 #endif // MODEPASPASWIDGET_H
